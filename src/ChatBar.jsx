f4322c7e4bc5b4dd5873d0c2876e78a517ab8e73
Geoff Phillips <geoffreymartinphillips@gmail.com>
2018-09-04 15:00:44 -0700

    Add hidden submit button

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 
-const ChatBar = (props) => {
+const buttonStyle = {
+  height: '0px', 
+  width: '0px', 
+  border: 'none', 
+  padding: '0px'
+}
+
+function ChatBar(props) {
   function onSubmit(event) {
     event.preventDefault();
-
     const message = event.target.elements.message;
     props.addNewMessage(props.currentUser, message.value);
-    console.log(message)
     message.value = '';
   }
 
   return (
     <footer className="chatbar">
-      <form onSubmit={ onSubmit }>
+      <form onSubmit={ onSubmit } >
         <input className="chatbar-username" defaultValue={ props.currentUser } placeholder="Your Name (Optional)" />
         <input name="message" className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input type="submit" style={buttonStyle} display="hidden" />
       </form>
     </footer>
   )
