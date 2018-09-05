350d6584c3549e8d43bd5f4a6dd986a10e21d045
Geoff Phillips <geoffreymartinphillips@gmail.com>
2018-09-04 17:10:14 -0700

    Users can change username

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -11,14 +11,15 @@ function ChatBar(props) {
   function onSubmit(event) {
     event.preventDefault();
     const message = event.target.elements.message;
-    props.addNewMessage(props.currentUser, message.value);
+    const username = event.target.elements.username.value;
+    props.addNewMessage(username, message.value);
     message.value = '';
   }
 
   return (
     <footer className="chatbar">
       <form onSubmit={ onSubmit } >
-        <input className="chatbar-username" defaultValue={ props.currentUser } placeholder="Your Name (Optional)" />
+        <input name="username" className="chatbar-username" defaultValue={ props.currentUser } placeholder="Your Name (Optional)" />
         <input name="message" className="chatbar-message" placeholder="Type a message and hit ENTER" />
         <input type="submit" style={buttonStyle} display="hidden" />
       </form>
